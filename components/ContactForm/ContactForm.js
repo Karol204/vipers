d165a2818467afe708b98e711719f350d6ccbd16
Karol204 <krogalski204@gmail.com>
2023-12-10 23:14:48 +0000

    reCapchat

--- components/ContactForm/ContactForm.js
+++ components/ContactForm/ContactForm.js
@@ -2,25 +2,33 @@
 import React, { useRef } from 'react'
 import styles from './ContactForm.module.css'
 import emailjs from '@emailjs/browser';
+import ReCAPTCHA from "react-google-recaptcha";
+
 
 
 const ContactForm = ({title, candidates}) => {
   const form = useRef();
+  const recaptcha = useRef();
 
 
   const sendEmail = (e) => {
     e.preventDefault();
-    console.log('bnbdb')
-
-    emailjs.sendForm('service_p400yaa', 'template_7bwnooi', form.current, process.env.PUBLIC_KEY)
+    const captchaValue = recaptcha.current.getValue()
+    if (!captchaValue) {
+      alert("Please verify you are not a robot");
+    } else { 
+      emailjs.sendForm('service_p400yaa', 'template_7bwnooi', form.current, process.env.PUBLIC_KEY)
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
       e.target.reset()
+    }
   };
 
+  
+  // process.env.PUBLIC_KEY
 
 
   return (
@@ -33,6 +41,8 @@ const ContactForm = ({title, candidates}) => {
 		  <input id="email" type="text" placeholder="E-MAIL" className={styles.input} name='email' required/>
 		  <textarea id="message" type="text" placeholder="MESSAGE" className={styles.textarea} name='message'></textarea>
       <input id="submit" type="submit" value="Send!" className={styles.input}/>
+      <ReCAPTCHA ref={recaptcha} sitekey={process.env.REACT_APP_SITE_KEY} />
+      
 
     </form>
     </div>
